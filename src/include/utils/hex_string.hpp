799bd28730e2dc46d4c594540b8d675e8e8e573d
Mike Chang <shox0909@gmail.com>
2025-07-31 13:50:09 +0800

    1. fix for compiling issue

--- src/include/utils/hex_string.hpp
+++ src/include/utils/hex_string.hpp
@@ -1,12 +1,14 @@
 #ifndef __HEX_STRING_HPP__
 #define __HEX_STRING_HPP__
 
+#include <cstdint>
 #include <iomanip>
 #include <sstream>
 #include <string>
 
-using namespace std;
+// using namespace std;
+std::string hexString(uint64_t value, uint64_t digits, bool ellipsis = false);
 
-string hexString(uint64_t value, uint64_t digits, bool ellipsis = false);
+// string hexString(uint64_t value, uint64_t digits, bool ellipsis = false);
 
 #endif
--- src/utils/hex_string.cpp
+++ src/utils/hex_string.cpp
@@ -1,8 +1,8 @@
 #include "utils/hex_string.hpp"
 
-string hexString(uint64_t value, uint64_t digits, bool ellipsis) {
+std::string hexString(uint64_t value, uint64_t digits, bool ellipsis) {
   std::stringstream stream;
-  stream << setfill('0') << setw(digits) << hex << uppercase << value;
+  stream << std::setfill('0') << std::setw(digits) << std::hex << std::uppercase << value;
   std::string valueStr(stream.str());
   if (ellipsis == true) {
     uint64_t valueLength = valueStr.length();
